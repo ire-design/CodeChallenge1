7f675dc20d1b6c6485599c4041f80e85d2f6f5af
ire-design <irenemwikalii04@gmail.com>
2024-06-28 23:15:49 +0300

    changes made

--- Student.js
+++ Student.js
@@ -5,33 +5,38 @@ Write a function that prompts the user to input student marks. The input should 
         A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
 */
 function studentGrade(){
-
+    //prompt the user to input a score
     let score = prompt("Please enter the student's score")
-    //to only work with integers.
+
+    //converts whatever the input is to an integer
     score = parseInt(score)
 
-    if (score >=80 && score < 100 ){
-            console.log(`Your score is: ${score} hence your grade is A`)
+    //score must be within the 0-100 range 
+    if (score >= 0 && score <= 100) {
 
-    }else if (score >=60 && score <= 79){
-            console.log(`Your score is: ${score} hence your grade is B`)
+        //grade according to the score
+        if (score >79 ){
+                console.log(`Your score is: ${score} hence your grade is A`)
 
-    }else if (score  > 49 && score <= 59 ){
-            console.log(`Your score is: ${score} hence your grade is C`)
+        }else if (score >=60 && score <= 79){
+                console.log(`Your score is: ${score} hence your grade is B`)
 
-    }else if (score >= 40 && score <= 49){
-            console.log(`Your score is: ${score} hence your grade is D`)
+        }else if (score  > 49 && score <= 59 ){
+                console.log(`Your score is: ${score} hence your grade is C`)
 
-    }else if(score >=0 && score < 40){
-            console.log(`Your score is: ${score} hence your grade is E`)
-            
-    }else{
-            console.log("Out of range. please enter a number between 0 and 100!")
+        }else if (score >= 40 && score <= 49){
+                console.log(`Your score is: ${score} hence your grade is D`)
+
+        }else {
+                console.log(`Your score is: ${score} hence your grade is E`)
+        }       
+        
+    } else {
+                console.log("Out of range. please enter a number between 0 and 100!")
     }
-
-
+    
 }
-
+//callfunction
 studentGrade();
 
 
